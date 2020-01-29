c8c3197dc3cbb7c8d6cac404e7b9c420d3c7d018
JDBU-Games <47919909+jdbugames@users.noreply.github.com>
2020-01-28 20:32:31 -0500

    Preparando el entorno VR

---
+++ Assets/Main/Scripts/RotateCubeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCubeController : MonoBehaviour
+{
+    [SerializeField]
+    private float fl_SpinForce = 45f;
+
+    [SerializeField]
+    private bool bl_IsSpinning = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bl_IsSpinning)
+        {
+            transform.Rotate(0, fl_SpinForce * Time.deltaTime, 0);
+        }
+        else if(!bl_IsSpinning)
+        {
+            transform.Rotate(0, 0, 0);
+        }
+
+    }
+
+    public void ChangeSpin()
+    {
+        bl_IsSpinning = !bl_IsSpinning;
+    }
+}
