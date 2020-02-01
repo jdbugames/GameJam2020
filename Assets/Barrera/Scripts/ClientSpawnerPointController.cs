de85a7d812ba9287a7ab06a3c2aeff7d7149c749
Euclides <eneye280@gmail.com>
2020-02-01 15:34:25 -0500

    Not Change

---
+++ Assets/Barrera/Scripts/ClientSpawnerPointController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSpawnerPointController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject client;
+
+    [SerializeField]
+    private Transform pointDestination;
+
+    [SerializeField]
+    private Transform pointDelivery;
+
+    [SerializeField]
+    private float spawnerTime;
+
+    [SerializeField]
+    private int numberClients;
+
+    [SerializeField]
+    private bool canSpawner;
+
+
+    private void Start()
+    {
+        StartCoroutine(RatSpawn());
+    }
+
+    IEnumerator RatSpawn()
+    {
+        int ratsSpawned = 0;
+        while (canSpawner && ratsSpawned < numberClients)
+        {
+            Instantiate(client, this.transform.position, Quaternion.identity);
+            ratsSpawned++;
+            canSpawner = !canSpawner;
+
+            yield return new WaitForSeconds(spawnerTime);
+            canSpawner = !canSpawner;
+        }
+    }
+}
