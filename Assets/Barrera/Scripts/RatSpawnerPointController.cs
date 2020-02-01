d99d93e575457bb8032a1149ea0820ef102f487e
JDBU-Games <47919909+jdbugames@users.noreply.github.com>
2020-02-01 02:59:13 -0500

    Rat and RatSpawnerPoint Controllers

---
+++ Assets/Barrera/Scripts/RatSpawnerPointController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatSpawnerPointController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rat;
+
+    [SerializeField]
+    private float spawnerTime;
+
+    [SerializeField]
+    private int numberRats;
+
+    [SerializeField]
+    private bool canSpawner;
+
+
+    private void Start()
+    {
+        StartCoroutine(RatSpawn());
+    }
+
+    IEnumerator RatSpawn()
+    {
+        int ratsSpawned = 0;
+        while (canSpawner && ratsSpawned < numberRats)
+        {
+            Instantiate(rat, this.transform.position, Quaternion.identity);
+            ratsSpawned++;
+            canSpawner = !canSpawner;
+
+            yield return new WaitForSeconds(spawnerTime);
+            canSpawner = !canSpawner;
+        }
+    }
+}
