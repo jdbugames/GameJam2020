4d9b16260d9ec7667f526f4e2725b4104dec113c
JDBU-Games <47919909+jdbugames@users.noreply.github.com>
2020-02-01 18:16:32 -0500

    Insect IA

--- Assets/Barrera/Scripts/ClientSpawnerPointController.cs
+++ Assets/Barrera/Scripts/ClientSpawnerPointController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ClientSpawnerPointController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject client;
+    private GameObject[] allClients;
+
+    [SerializeField]
+    private GameObject[] allObjects;
 
     [SerializeField]
     private Transform pointDestination;
@@ -22,6 +25,8 @@ public class ClientSpawnerPointController : MonoBehaviour
     [SerializeField]
     private bool canSpawner;
 
+    public GameObject clientInPointDestination;
+
 
     private void Start()
     {
@@ -30,11 +35,20 @@ public class ClientSpawnerPointController : MonoBehaviour
 
     IEnumerator RatSpawn()
     {
-        int ratsSpawned = 0;
-        while (canSpawner && ratsSpawned < numberClients)
+        int clientSpawned = 0;
+        while (canSpawner && clientSpawned < numberClients)
         {
-            Instantiate(client, this.transform.position, Quaternion.identity);
-            ratsSpawned++;
+            int clientRandom = Random.Range(0, allClients.Length);
+            GameObject clientSpawn = Instantiate(allClients[clientRandom], this.transform.position, Quaternion.identity);
+
+            int objectRandom = Random.Range(0, allObjects.Length);
+            GameObject objectRandomSelected = Instantiate(allObjects[objectRandom], this.transform.position, Quaternion.identity);
+            clientSpawn.GetComponent<Client>().objectDeliver = objectRandomSelected;
+
+            clientSpawn.GetComponent<Client>().pointDestination = pointDestination;
+            clientSpawn.GetComponent<Client>().pointDelivery = pointDelivery;
+            clientSpawn.transform.Rotate(0f, 180f, 0f); 
+            clientSpawned++;
             canSpawner = !canSpawner;
 
             yield return new WaitForSeconds(spawnerTime);
