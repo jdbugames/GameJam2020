fc149a11ede37022e256d01afb24058e235b4fcd
Euclides <eneye280@gmail.com>
2020-01-31 23:50:40 -0500

    Update project
    * Add Folder Developer

---
+++ Assets/DEVELOPER GAME/Script/@Player/MovementPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    internal static MovementPlayer movementPlayer;
+    internal static MovementPlayer instance
+    {
+        get
+        {
+            if(!movementPlayer)
+            {
+                movementPlayer = FindObjectOfType<MovementPlayer>();
+
+                if(!movementPlayer)
+                {
+                    var singleton = new GameObject(typeof(MovementPlayer).ToString());
+                    movementPlayer = singleton.AddComponent<MovementPlayer>();
+                }
+            }
+            return movementPlayer;
+        }
+    }
+    
+    [SerializeField] internal Vector2 direction;
+    [SerializeField] internal Vector3 movement;
+    [SerializeField] internal Animator animatorPlayer;
+
+    [Range(0,100)]
+    [SerializeField] internal float speed;
+
+    private void Awake()
+    {
+        animatorPlayer = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        CallMovementPlayer();
+    }
+
+    public void CallMovementPlayer()
+    {
+        float h = direction.x;
+        float v = direction.y;
+
+        movement = new Vector3();
+        movement.Set(h, 0, v);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        if (animatorPlayer == null)
+            return;
+
+        animatorPlayer.SetFloat("vertical", direction.y);
+        animatorPlayer.SetFloat("horizontal", direction.x);
+    }
+}
---
+++ Assets/DEVELOPER GAME/Script/@Player/PlayerInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider), typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private MovementPlayer movementPlayer;
+
+    internal InputPlayer controls;
+
+    private void Awake()
+    {
+        movementPlayer = GetComponent<MovementPlayer>();
+        
+        controls = new InputPlayer();
+
+        controls.Player.Movement.performed += ctx => movementPlayer.direction = ctx.ReadValue<Vector2>();
+    }
+    private void OnDisable() => controls.Disable();
+    private void OnEnable() => controls.Enable();
+}
